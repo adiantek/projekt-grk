aff8dc0c224a309baf608c84a71a63a116c90d7c
Adrian Antkowiak <adiantek@gmail.com>
2022-01-19 03:07:53 +0100

    Libraries (zlib, libpng, PhysX) (#53)
    * move libraries to another directory
    
    * forgot about SOIL xD
    
    * add ZLIB
    
    * add libpng
    
    * remove SOIL
    
    * fix for emscripten, fix some warnings
    
    * change to debug & add physx
    
    * fix for emscripten (wtf?)

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,11 @@
 #include "Render_Utils.h"
 #include "Texture.h"
 
+#include <zlib/zlib.h>
+#include <png/png.h>
+#include "PxPhysics.h"
+#include "foundation/PxMathUtils.h"
+
 Core::RenderContext shipContext;
 Core::RenderContext sphereContext;
 Core::RenderContext sphereContext2;
@@ -255,6 +260,13 @@ void init() {
 int main(int argc, char **argv)
 {
 	vertex::VertexFormats_load();
+	LOGD("ZLIB version: %s", zlibVersion());
+	LOGD("libpng version: %s", png_get_libpng_ver(NULL));
+	{
+		physx::PxTransform transform = PxTransformFromPlaneEquation(physx::PxPlane(1, 1, 0, 2));
+		LOGD("Physics success: %f %f %f", transform.p.x, transform.p.y, transform.p.z);
+	}
+
 	glfwSetErrorCallback(glfw_error_callback);
     if (glfwInit() != GL_TRUE)
     {
