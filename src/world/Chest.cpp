446ec5d158bcaf75a99ca6ea08595ae1e2a83b8e
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-02-10 14:59:08 +0100

    chest PBR

--- src/world/Chest.cpp
+++ src/world/Chest.cpp
@@ -27,16 +27,68 @@ void Chest::update() {
 
 void Chest::draw(glm::mat4 mat) {
     ResourceLoader *res = resourceLoaderExternal;
-    glUseProgram(res->p_simple_tex_shader);
-    glUniformMatrix4fv(res->p_simple_tex_shader_uni_transformation, 1, GL_FALSE, glm::value_ptr(mat * this->model));
-    glUniform1i(res->p_simple_tex_shader_uni_textureSampler, 0);
+    glUseProgram(res->p_color_armature);
+    glUniformMatrix4fv(res->p_color_armature_uni_modelMatrix, 1, GL_FALSE, glm::value_ptr(this->model));
+    glUniformMatrix4fv(res->p_color_armature_uni_modelViewProjectionMatrix, 1, GL_FALSE, glm::value_ptr(mat * this->model));
+    std::vector<glm::mat4> test(50);
+    glUniformMatrix4fv(res->p_color_armature_uni_jointTransforms, 50, GL_FALSE, glm::value_ptr(test[0]));
+    glUniform1i(res->p_color_armature_uni_albedoTexture, 0);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_reinforcment_albedo);
+    glUniform1i(res->p_color_armature_uni_aoTexture, 1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_reinforcment_ao);
+    glUniform1i(res->p_color_armature_uni_metallicTexture, 2);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_reinforcment_metallic);
+    glUniform1i(res->p_color_armature_uni_normalTexture, 3);
+    glActiveTexture(GL_TEXTURE3);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_reinforcment_normal);
+    glUniform1i(res->p_color_armature_uni_roughnessTexture, 4);
+    glActiveTexture(GL_TEXTURE4);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_reinforcment_roughness);
     Core::DrawContext(*(res->m_props_chest->getMeshes()[0]->getRenderContext()));
+    glUniform1i(res->p_color_armature_uni_albedoTexture, 0);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_wood_albedo);
+    glUniform1i(res->p_color_armature_uni_aoTexture, 1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_wood_ao);
+    glUniform1i(res->p_color_armature_uni_metallicTexture, 2);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_wood_metallic);
+    glUniform1i(res->p_color_armature_uni_normalTexture, 3);
+    glActiveTexture(GL_TEXTURE3);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_wood_normal);
+    glUniform1i(res->p_color_armature_uni_roughnessTexture, 4);
+    glActiveTexture(GL_TEXTURE4);
+    glBindTexture(GL_TEXTURE_2D, res->tex_props_chest_chest_wood_roughness);
     Core::DrawContext(*(res->m_props_chest->getMeshes()[1]->getRenderContext()));
 }
 
 void Chest::drawShadow(glm::mat4 mat) {
+    glm::mat4 viewMatrix = mat;
+    glm::mat4 modelMatrix = this->model;
+    ResourceLoader *res = resourceLoaderExternal;
+    std::vector<Mesh *> meshes = res->m_props_chest->getMeshes();
+
+    glm::mat4 modelViewProjectionMatrix = viewMatrix * modelMatrix;
+    std::vector<glm::mat4> test(50);
+
+
+    // Iterate over meshes
+    for (int i = 0; i < meshes.size(); i++) {
+        Mesh* mesh = meshes[i];
+
+        Core::RenderContext* context = mesh->getRenderContext();
+
+        glUseProgram(resourceLoaderExternal->p_armature_shadow);
+
+        // Set transformation
+        glUniformMatrix4fv(resourceLoaderExternal->p_armature_shadow_uni_modelMatrix, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+        glUniformMatrix4fv(resourceLoaderExternal->p_armature_shadow_uni_modelViewProjectionMatrix, 1, GL_FALSE, glm::value_ptr(modelViewProjectionMatrix));
+        glUniformMatrix4fv(resourceLoaderExternal->p_armature_shadow_uni_jointTransforms, 50, GL_FALSE, glm::value_ptr(test[0]));
+
+        Core::DrawContext(*context);
+    }
 }
