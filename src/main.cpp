e573ae53a96763c414792e2b7f4c2847f421e527
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-02-10 23:48:55 +0100

    fish swim closer to player

--- src/main.cpp
+++ src/main.cpp
@@ -192,25 +192,25 @@ void init() {
     Random random(0L);
 
     for (int i=0; i<BOIDS_AMOUNT / 4;++i) {
-        Boids<Pilotfish> *boid = new Boids<Pilotfish>(BOIDS_SIZE, glm::vec3(random.nextFloat(-100.0f, 100.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-100.0f, 100.0f)), w);
+        Boids<Pilotfish> *boid = new Boids<Pilotfish>(BOIDS_SIZE, glm::vec3(random.nextFloat(-50.0f, 50.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-50.0f, 50.0f)), w);
         waterObject->addWorldObject((world::Object3D *)boid);
         boids.push_back(boid);
     }
 
     for (int i=0; i<BOIDS_AMOUNT / 4;++i) {
-        Boids<Barracuda> *boid = new Boids<Barracuda>(BOIDS_SIZE, glm::vec3(random.nextFloat(-100.0f, 100.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-100.0f, 100.0f)), w);
+        Boids<Barracuda> *boid = new Boids<Barracuda>(BOIDS_SIZE, glm::vec3(random.nextFloat(-50.0f, 50.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-50.0f, 50.0f)), w);
         waterObject->addWorldObject((world::Object3D *)boid);
         boids2.push_back(boid);
     }
 
     for (int i=0; i<BOIDS_AMOUNT / 4;++i) {
-        Boids<RedSnapper> *boid = new Boids<RedSnapper>(BOIDS_SIZE, glm::vec3(random.nextFloat(-100.0f, 100.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-100.0f, 100.0f)), w);
+        Boids<RedSnapper> *boid = new Boids<RedSnapper>(BOIDS_SIZE, glm::vec3(random.nextFloat(-50.0f, 50.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-50.0f, 50.0f)), w);
         waterObject->addWorldObject((world::Object3D *)boid);
         boids3.push_back(boid);
     }
 
     for (int i=0; i<BOIDS_AMOUNT / 4;++i) {
-        Boids<Golden> *boid = new Boids<Golden>(BOIDS_SIZE, glm::vec3(random.nextFloat(-100.0f, 100.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-100.0f, 100.0f)), w);
+        Boids<Golden> *boid = new Boids<Golden>(BOIDS_SIZE, glm::vec3(random.nextFloat(-50.0f, 50.0f), random.nextFloat(170.0f, 190.0f), random.nextFloat(-50.0f, 50.0f)), w);
         waterObject->addWorldObject((world::Object3D *)boid);
         boids4.push_back(boid);
     }
